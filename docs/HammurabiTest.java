6c73e86e3db8c299b1c64964db1de03baf8feb26
nigel <bynum1001@gmail.com>
2023-07-15 16:31:30 -0400

    test3

--- docs/HammurabiTest.java
+++ docs/HammurabiTest.java
@@ -2,14 +2,21 @@ package hammurabi.docs;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.jupiter.api.Assertions.*;
- public class Hammurabi {
+
+public class Hammurabi {
 
      private Hammurabi hammurabi;
 
      @Before
      public void setup() {
          hammurabi = new Hammurabi();
+     }
+     @Test
+     public void askHowManyAcresToBuyTest() {
+         Integer input = 10;
+         Integer expected = 10;
+         Integer actual = hammurabi.askHowManyAcresToBuy(input);
+         Assert.assertEquals(expected, actual);
      }
 
      @Test
@@ -19,7 +26,59 @@ import static org.junit.jupiter.api.Assertions.*;
          Integer actual = hammurabi.askHowManyAcresToSell(input);
          Assert.assertEquals(expected, actual);
      }
- }
+
+    @Test
+    public void askHowMuchGrainToFeedPeopleTest() {
+        Integer input = 3;
+        Integer expected = 3;
+        Integer actual = hammurabi.askHowMuchGrainToFeedPeople(input);
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void askHowManyAcresToPlantTest() {
+        Integer input = 2;
+        Integer expected = 2;
+        Integer actual = hammurabi.askHowManyAcresToPlant(input);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void plagueDeathsTest() {
+        Integer input = 65;
+        Integer expected = 65;
+        Integer actual = hammurabi.plagueDeaths();t(input);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void starvationDeathsTest() {
+        Integer input = 65;
+        Integer expected = 65;
+        Integer actual = hammurabi.starvationDeaths(input);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void uprisingTest() {
+        Integer input = 50;
+        Integer expected = True;
+        Integer actual = hammurabi.uprising(input);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void  immigrantsTest() {
+        Integer input = 50;
+        Integer expected = True;
+        Integer actual = hammurabi. immigrants(input);
+        Assert.assertEquals(expected, actual);
+    }
+
+
+
+
+
+        }
 
 
    /* int askHowManyAcresToBuy(int price, int bushels)
